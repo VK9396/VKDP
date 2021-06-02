9988a06a12dae258afb85c76fcc7385dc4372975
vivek <vivek@DESKTOP-86B2GQB>
2021-06-02 17:44:00 +0530

    Using egar Instantation by using new keyword

--- 1_SingleTonDP/src/com/itsp/stdp/Printer.java
+++ 1_SingleTonDP/src/com/itsp/stdp/Printer.java
@@ -2,17 +2,20 @@ package com.itsp.stdp;
 
 public class Printer {
 	
-	private static  Printer INSTANCE  ;
+	private static  Printer INSTANCE = new Printer() ;// egar Instantiation
 	
 	private Printer() {
-		
+		System.out.println("private 0-arg Constructor");
 	}
 	
 	public static Printer getInstance() {
-		//singletone logic
+		/*singletone logic
 		if(INSTANCE==null)
 			INSTANCE = new Printer();
 		 return  INSTANCE;
+		*/
+		
+		return INSTANCE;
 		
 	}
 	
